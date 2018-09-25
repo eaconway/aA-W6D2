260b26288819acd2879b5fffcfb58ec8e22d39ac
Rola1993 <rola.uiuc@gmail.com>
2018-09-25 11:51:42 -0700

    Add webpack dependencies

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -1,6 +1,7 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');// require appropriate file
+const Game = require("../solution/game.js");// require appropriate file
 
 $( () => {
   // Your code here
+  
 });
